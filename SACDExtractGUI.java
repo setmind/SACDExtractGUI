97d05f9581a8c737c8d481a8d92dea10f8766a3e
setmind <setmind@users.noreply.github.com>
2018-10-12 19:59:05 -0700

    Catching quit command for mac

--- SACDExtractGUI.java
+++ SACDExtractGUI.java
@@ -1,5 +1,8 @@
 class SACDExtractGUI{
     public static void main(String args[]){
+        if(OSDetect.isMac()){
+            System.setProperty("apple.eawt.quitStrategy", "CLOSE_ALL_WINDOWS");
+        }
         AppFrame app = AppFrame.getInstance();
         app.setVisible(true);
     }
